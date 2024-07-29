56468705a4a0b7279b8fcd349e28cd54c38d02b9
Pedro Serey <serey.pedro@gmail.com>
2024-07-29 14:51:03 -0300

    add: snippet and other plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,55 +1,55 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+  -- You can also add or configure plugins by creating files in this `plugins/` folder
+  -- Here are some examples:
 
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
+  ---@type LazySpec
+  return {
 
----@type LazySpec
-return {
+    -- == Examples of Adding Plugins ==
 
-  -- == Examples of Adding Plugins ==
+    "andweeb/presence.nvim",
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "BufRead",
+      config = function() require("lsp_signature").setup() end,
+    },
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+    -- == Examples of Overriding Plugins ==
 
-  -- == Examples of Overriding Plugins ==
+    -- customize alpha options
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts)
+        -- customize the dashboard header
+        opts.section.header.val = {
+          " █████  ███████ ████████ ██████   ██████",
+          "██   ██ ██         ██    ██   ██ ██    ██",
+          "███████ ███████    ██    ██████  ██    ██",
+          "██   ██      ██    ██    ██   ██ ██    ██",
+          "██   ██ ███████    ██    ██   ██  ██████",
+          " ",
+          "    ███    ██ ██    ██ ██ ███    ███",
+          "    ████   ██ ██    ██ ██ ████  ████",
+          "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+          "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+          "    ██   ████   ████   ██ ██      ██",
+        }
+        return opts
+      end,
+    },
 
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }
-      return opts
-    end,
-  },
+    -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+    {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+        -- add more custom luasnip configuration such as filetype extend or custom snippets
+        local luasnip = require "luasnip"
+        luasnip.filetype_extend("javascript", { "javascriptreact" })
 
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
+        -- load snippets paths
+        require("luasnip.loaders.from_vscode").lazy_load({
+          paths = { vim.fn.stdpath("config") .. "/snippets" },
+      })
     end,
   },
 
